6fb328f7b441af8b644185bfa09a944a8c5e7ef1
ChaigneauDoryan <doryan.chaigneau@hotmail.fr>
2025-08-04 18:36:51 +0200

    Fix: Make client directory clickable on GitHub

---
+++ client/src/app/auth/login/page.tsx
@@ -0,0 +1,33 @@
+
+import { createClient } from '@/lib/supabase/server'
+import { cookies, headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default function LoginPage() {
+  const handleSignIn = async () => {
+    'use server'
+    const cookieStore = cookies()
+    const supabase = createClient(cookieStore)
+
+    const origin = headers().get('host');
+    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+    const redirectToUrl = `${protocol}://${origin}/auth/callback`;
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: redirectToUrl,
+      },
+    })
+
+    if (data.url) {
+      redirect(data.url)
+    }
+  }
+
+  return (
+    <form action={handleSignIn}>
+      <button>Se connecter avec Google</button>
+    </form>
+  )
+}
